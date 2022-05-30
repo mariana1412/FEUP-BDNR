095c72d69ea19215dd7a51ede409020e0b497787
antbz <antonio.bezerra@fe.up.pt>
2022-05-30 22:40:40 +0100

    add backend for admin history

--- project/website/server/controllers/purchases.js
+++ project/website/server/controllers/purchases.js
@@ -1,5 +1,16 @@
 const { purchases, clients, products } = require("../config");
 
+async function getProducts(purchases_query) {
+    
+    for (let purchase in purchases_query) {
+        for (let product in purchases_query[purchase].products) {
+            const products_session = products.openSession()
+            const product_query = await products_session.advanced.rawQuery(`from @all_docs where id == "${purchases_query[purchase].products[product]}"`).first()
+            purchases_query[purchase].products[product] = product_query
+        }
+    }
+}
+
 async function getHistory(req, res){
     const { name, type } = req.query
     
@@ -8,7 +19,6 @@ async function getHistory(req, res){
     if (type === 'client') {
         const clients_session = clients.openSession()
         const purchases_session = purchases.openSession()
-        const products_session = products.openSession()
         try {
             const client_query = await clients_session.advanced.rawQuery(`from "@all_docs" where name == "${name}" select id`).first()
             const client_id = client_query.id
@@ -16,18 +26,20 @@ async function getHistory(req, res){
             const purchases_query = await purchases_session.query(purchases).whereEquals('client', client_id).orderByDescending('date').all()
             console.log(purchases_query)
 
-            for (let purchase in purchases_query) {
-                for (let product in purchases_query[purchase].products) {
-                    const product_query = await products_session.advanced.rawQuery(`from @all_docs where id == "${purchases_query[purchase].products[product]}"`).first()
-                    purchases_query[purchase].products[product] = product_query
-                }
-            }
+            await getProducts(purchases_query)
 
             return res.status(200).send(purchases_query)
         } catch (e) {
             console.log(e);
             return res.status(404).send("No such client")
         }
+    } else if (type === 'admin') {
+        const purchases_session = purchases.openSession()
+        const purchases_query = await purchases_session.query(purchases).take(10).all()
+
+        await getProducts(purchases_query)
+
+        return res.status(200).send(purchases_query)
     }
 }
 
