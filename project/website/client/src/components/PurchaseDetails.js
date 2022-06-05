f64b58a9624c253c9fff58596cba8478791c97d8
antbz <antonio.bezerra@fe.up.pt>
2022-06-05 17:35:40 +0100

    finish order history

--- project/website/client/src/components/PurchaseDetails.js
+++ project/website/client/src/components/PurchaseDetails.js
@@ -2,27 +2,35 @@ import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 
 export default function PurchaseDetails({ purchase }) {
-  const date = new Date(purchase.date);
+  const date = new Date(purchase.orderDate);
 
   return (
     <div className="mb-4">
       <h3>{date.getDate()}/{date.getMonth() + 1}/{date.getFullYear()}</h3>
       <Row className="font-weight-bold">
         <Col><strong>Product</strong></Col>
-        <Col><strong>Price</strong></Col>
         <Col><strong>Store</strong></Col>
+        <Col><strong>Unit Price</strong></Col>
+        <Col><strong>Quantity</strong></Col>
+        <Col><strong>Total</strong></Col>
       </Row>
       {
-        purchase.products.map((product) => (
-          <Row>
+        purchase.lines.map((product) => (
+          <Row key={product.productId}>
+            <Col className="text-truncate">
+              {product.productName}
+            </Col>
             <Col>
-              {product.name}
+              {product.store}
+            </Col>
+            <Col>
+              {parseFloat(product.unitPrice).toFixed(2)}
+            </Col>
+            <Col>
+              {product.quantity}
             </Col>
             <Col>
               {parseFloat(product.price).toFixed(2)}
-            </Col>
-            <Col>
-              {product['@metadata']['@collection']}
             </Col>
           </Row>
         ))
